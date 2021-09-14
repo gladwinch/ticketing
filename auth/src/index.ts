c343f0f2e6c36d03914837e48d2fd74b7bfd6a07
gladwinch20@gmail.com <gladwinch20@gmail.com>
2021-09-14 12:44:27 +0530

    ingress setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send("hi there!")
+})
+
 app.listen(3000, () => {
     console.log("Listening of port 3000")
 })
